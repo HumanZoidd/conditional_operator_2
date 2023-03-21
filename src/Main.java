41893b8e36105b6b7c55708dc8d45a9e4fe5d5bc
Sergey Kharitonov <haritonov_s@mail.ru>
2023-03-21 21:39:31 +0400

    hw done

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-        // task3();
-        // task4();
-    }
+        task3();
+        task4();
+        task5();
 
+    }
     public static void task1() {
         System.out.println("Задача 1");
         byte clientOS = 3;
@@ -17,7 +18,6 @@ public class Main {
             System.out.println("«Иные OS не поддерживаются»");
         }
     }
-
     public static void task2() {
         System.out.println("Задача 2");
         byte clientOS = 3;
@@ -34,6 +34,53 @@ public class Main {
         }
         if (clientOS != 0 && clientOS != 1) {
             System.out.println("Другие ОС не поддерживаются");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int year = 2021;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високостным");
+        } else {
+            System.out.println(year + " год не является високостным");
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        byte deliveryDistance = 20;
+        if (deliveryDistance < 20) {
+            System.out.println("Досавка карты займёт 1 сутки");
+        }
+        if (deliveryDistance <= 60 && deliveryDistance >= 20) {
+            System.out.println("Доставка карты займёт 2 суток");
+        }
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Доставка карты займет 3 суток");
+        } else if (deliveryDistance > 100) {
+            System.out.println("Доставка карты невозможна");
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        byte monht = 1;
+        switch (monht) {
+            case 1, 2, 12:
+                System.out.println("Winter");
+                break;
+            case 3, 4, 5:
+                System.out.println("Spring");
+                break;
+            case 6, 7, 8:
+                System.out.println("Summer");
+                break;
+            case 9, 10, 11:
+                System.out.println("Autumn");
+                break;
+            default:
+                System.out.println("Это точно не на Земле");
         }
     }
 }
