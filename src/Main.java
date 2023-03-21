9ed26f9fcec7b2b67b577fcf6b74d7712357f3b1
Sergey Kharitonov <haritonov_s@mail.ru>
2023-03-21 12:43:56 +0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        // task3();
+        // task4();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        byte clientOS = 3;
+        if (clientOS == 0) {
+            System.out.println("«Установите версию приложения для iOS по ссылке»");
+        } else if (clientOS == 1) {
+            System.out.println("«Установите версию приложения для Android по ссылке»");
+        } else {
+            System.out.println("«Иные OS не поддерживаются»");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        byte clientOS = 3;
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("«Установите версию приложения для iOS по ссылке»");
+        } else if (clientDeviceYear < 2015) {
+            System.out.println("«Установите облегченную версию приложения для iOS по ссылке»");
+        }
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("«Установите версию приложения для Android по ссылке»");
+        } else if ((clientDeviceYear < 2015)) {
+            System.out.println("«Установите облегченную версию приложения для Android по ссылке»");
+        }
+        if (clientOS != 0 && clientOS != 1) {
+            System.out.println("Другие ОС не поддерживаются");
+        }
     }
 }
+
